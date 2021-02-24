c867581ba8b344e242bf90e7bf543ee134dd2be4
AssADev <gravelteo.pro@gmail.com>
2021-02-24 19:46:33 +0100

    Header Animations

--- files/js/app.js
+++ files/js/app.js
@@ -1,69 +1,95 @@
-// Header Title (Harry Potter) :
-const harryPotterTitle = document.querySelector("header h1");
-harryPotterTitle.innerHTML = harryPotterTitle.textContent.replace(/\S/g, `<span class="letter">$&</span>`);
+// Functions Animations :
+function letterizeSpan(element) {
+    return element.textContent.replace(/\S/g, `<span class="letter">$&</span>`);
+}
+
+// Custom Letterize :
+const potterEnigmaTitle = document.querySelector("header .title h1");
+potterEnigmaTitle.innerHTML = letterizeSpan(potterEnigmaTitle);
+
+const potterEnigmaDesc = document.querySelector("header .title p");
+potterEnigmaDesc.innerHTML = letterizeSpan(potterEnigmaDesc);
 
 // Introduction :
-// anime
-//     .timeline({ loop: false })
-//     .add(
-//         {
-//             targets: "h1 .letter",
-//             duration: 2000,
-//             skew: [15, 0],
-//             translateX: [-40, 0],
-//             translateY: [-180, 100],
-//             rotateY: [-90, 0],
-//             easing: "easeOutElastic(0.7, 1)",
-//             delay: (el, i) => 100 + 40 * i,
-//         },
-//         1000
-//     )
-//     .add(
-//         {
-//             targets: ".apparition",
-//             duration: 1250,
-//             scaleY: [1, 0],
-//             easing: "easeOutExpo",
-//         },
-//         "-=1000"
-//     )
-//     .add(
-//         {
-//             targets: "h1 .letter",
-//             duration: 2000,
-//             translateY: [100, 0],
-//             easing: "easeOutElastic(0.7, 1)",
-//             delay: (el, i) => 100 + 40 * i,
-//         },
-//         "-= 1000"
-//     )
-//     .add(
-//         {
-//             targets: "p.desc",
-//             duration: 2000,
-//             translateY: [40, 0],
-//             rotateX: [-45, 0],
-//             opacity: [0, 1],
-//             easing: "easeOutExpo",
-//         },
-//         "-= 1800"
-//     )
-//     .add(
-//         {
-//             targets: "header a",
-//             duration: 1200,
-//             translateY: [40, 0],
-//             opacity: [0, 1],
-//             easing: "easeOutExpo",
-//         },
-//         "-= 1800"
-//     )
-//     .add(
-//         {
-//             targets: ".scroll_visit i",
-//             duration: 1200,
-//             scale: [0, 1],
-//             easing: "easeOutExpo",
-//         },
-//         "-= 1600"
-//     );
+anime
+    .timeline({ loop: false })
+    .add(
+        {
+            targets: ".title",
+            duration: 2000,
+            height: ["100%", "40%"],
+            easing: "easeOutExpo",
+        },
+        2800
+    )
+    .add(
+        {
+            targets: "header .title h1 .letter",
+            duration: 2000,
+            opacity: [0, 1],
+            rotate: [-10, 0],
+            translateY: [-60, 0],
+            easing: "easeOutElastic(0.7, 1)",
+            delay: anime.stagger(60),
+        },
+        600
+    )
+    .add(
+        {
+            targets: "header .title p .letter",
+            duration: 2000,
+            opacity: [0, 1],
+            skew: [15, 0],
+            translateY: [20, 0],
+            easing: "easeOutElastic(0.7, 1)",
+            delay: anime.stagger(40),
+        },
+        1100
+    )
+    .add(
+        {
+            targets: ".left_side_inner",
+            duration: 1800,
+            translateX: ["100%", 0],
+            easing: "cubicBezier(0.5, 0, 0, 1)",
+        },
+        3000
+    )
+    .add(
+        {
+            targets: ".right_side_inner",
+            duration: 1800,
+            translateX: ["-100%", 0],
+            easing: "cubicBezier(0.5, 0, 0, 1)",
+        },
+        3000
+    )
+    .add(
+        {
+            targets: ".paragraphe_symbol",
+            duration: 1800,
+            opacity: [0, 1],
+            rotate: [0, "90deg"],
+            easing: "cubicBezier(0.5, 0, 0, 1)",
+        },
+        3100
+    )
+    .add(
+        {
+            targets: ".separator .line",
+            duration: 1800,
+            opacity: [0, 1],
+            scaleX: [0, 1],
+            easing: "cubicBezier(0.5, 0, 0, 1)",
+        },
+        3100
+    )
+    .add(
+        {
+            targets: ".aside_link",
+            duration: 2000,
+            translateY: ["-100%", 0],
+            easing: "easeOutExpo",
+        },
+        3800
+    );
